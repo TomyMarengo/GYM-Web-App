50f1616a0da415b50eb54ee0425b8d8872bb8c22
ichayer <ivan.chayer@hotmail.com>
2022-09-18 20:02:00 -0300

    Implement initial setup for RegisterPage

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import HomePage from '../pages/HomePage';
 import WorkoutsPage from "../pages/WorkoutsPage";
+import RegisterPage from "../pages/RegisterPage";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
     routes: [
         { path: '/', component: HomePage },
-        { path: '/entrenamientos', component: WorkoutsPage}
+        { path: '/entrenamientos', component: WorkoutsPage},
+        { path: '/registrarse', component: RegisterPage}
     ]
 });
 
